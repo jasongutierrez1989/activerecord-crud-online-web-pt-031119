a843631f8bf2df8f720c58d7f658e001e87bb93a
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-06 05:02:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,11 +35,11 @@ def can_be_created_in_a_block(args = nil)
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first#.title
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last#.title
+  Movie.last
 end
 
 def can_get_size_of_the_database
@@ -47,7 +47,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find(1).title
+  Movie.find(id: 1)#.title
 end
 
 def can_find_by_multiple_attributes
