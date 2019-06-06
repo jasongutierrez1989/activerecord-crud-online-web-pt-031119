b5ba4e8e9b9ac0166d3a95d5f5ea2edaff143826
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-06 05:13:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
 
   Movie.create do |m|
-    m.title = "The Room"
+    m.title = "Home Alone"
     m.release_date = 1990
   end
 end
